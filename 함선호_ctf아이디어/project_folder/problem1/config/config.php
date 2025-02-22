ca8649906de8190fc3faceb904d6436cda90437d
root <root@User.myguest.virtualbox.org>
2025-02-22 20:37:31 +0900

    fix:Connection

--- 함선호_ctf아이디어/project_folder/problem1/config/config.php
+++ 함선호_ctf아이디어/project_folder/problem1/config/config.php
@@ -1,5 +1,7 @@
+<?php
 $db_username = getenv('DB_USERNAME');
 $db_password = getenv('DB_PASSWORD');
 $db_name     = getenv('DB_NAME');
 $db_host     = getenv('DB_HOST');
 $port        = getenv('PORT');
+?>
